9ca5e02ad0194daca755342f443b321f0cce340c
Michael Fairley <michael.fairley@icloud.com>
2020-07-27 19:58:07 -0500

    reverted to in place model

--- src/sir.jl
+++ src/sir.jl
@@ -25,7 +25,6 @@ function prevalence_cost_model_sir(τ::Float64, r::Float64, ph::Array{Float64}; 
     tspan = (0, r+max_add_t)
     p = [μ, β0, γ0, ρ]
     prob = ODEProblem(sir_model!, u0, tspan, p)
-    # prob = sir_model_mt(u0, tspan, p)
     cb1 = PresetTimeCallback(change_times, affect!)
     cb2 = TerminateSteadyState(1e-8, 1e-6, termination)
     solve(prob, callback = CallbackSet(cb1, cb2))
@@ -38,30 +37,4 @@ function sir_model!(du, u, p, t)
     du[3] = γ*u[2] - μ*u[3]
     du[4] = exp(- t * ρ) * u[2] # discounted infection time
     nothing
-end
-
-function sir_model_mt(u0, tspan, p)
-    @parameters t, μ, β, γ, ρ
-    @variables S(t) I(t) R(t) c(t)
-    @derivatives D'~t
-    eqs = [
-        D(S) ~ μ - β*S*I - μ*S,
-        D(I) ~ β*S*I - γ*I - μ*I,
-        D(R) ~ γ*I - μ*R,
-        D(c) ~ exp(-t * ρ) * I
-    ]
-    sys = ODESystem(eqs)
-    u0 = [
-        S => u0[1]
-        I => u0[2]
-        R => u0[3]
-        c => u0[4]
-    ]
-    p = [
-        μ => p[1]
-        β => p[2]
-        γ => p[3]
-        ρ => p[4]
-    ]
-    prob = ODEProblem(sys, u0, tspan, p)
 end
