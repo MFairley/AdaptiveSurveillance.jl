9d7be0902ea6a79cf14eee6bcacfce7dcbe87c42
Michael Fairley <michael.fairley@icloud.com>
2020-07-27 19:06:03 -0500

    added print statements for travis

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,8 @@
+println("runtests.jl")
 using Test
 using Plots
+println("runtests.jl: packages loaded")
 using AdaptiveSurveillance
+println("runtests.jl: AdaptiveSurveillance loaded")
 
 include("sir_tests.jl")
