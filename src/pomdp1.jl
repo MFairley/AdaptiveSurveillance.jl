70564e81afe4544b21d50478d34e8b5828c02834
Michael Fairley <michael.fairley@icloud.com>
2020-08-28 12:08:01 -0500

    working pomdp implementation

--- src/pomdp1.jl
+++ src/pomdp1.jl
@@ -2,14 +2,7 @@ module pomdp1
 
 using POMDPs, POMDPModelTools, Distributions
 
-# const ν = 0.01
-# const p = [0.05, 0.1, 0.2, 1.0]
-# const N = length(p) + 1
-# const n_tests = 20
-# const sample_cost = 5.0
-# const delay_cost = 50.0
-
-struct SurveillancePOMDP <: POMDP{Bool, Symbol, Bool} # POMDP{State, Action, Observation}
+struct SurveillancePOMDP <: POMDP{Int64, Symbol, Int64}
     v::Float64 # probability of transitioning to epidemic
     p::Array{Float64} # prevalance sequence
     N::Int64
@@ -22,6 +15,7 @@ end
 # States
 POMDPs.states(pomdp::SurveillancePOMDP) = collect(1:pomdp.N) # N is the terminal state
 POMDPs.stateindex(pomdp::SurveillancePOMDP, s::Int64) = s
+POMDPs.isterminal(pomdp::SurveillancePOMDP, s::Int64) = s == pomdp.N
 
 # Actions
 POMDPs.actions(pomdp::SurveillancePOMDP) = [:stop, :sample, :continue]
@@ -45,44 +39,38 @@ function POMDPs.transition(pomdp::SurveillancePOMDP, s::Int64, a::Symbol)
         return SparseCat([1, 2], [1 - pomdp.v, pomdp.v])
     elseif 1 < s < (pomdp.N - 1)
         return Deterministic(s + 1)
-    else
-        return Deterministic(pomdp.N - 1)
     end
+    return Deterministic(pomdp.N - 1)
 end
 
 # Observations
 POMDPs.observations(pomdp::SurveillancePOMDP) = collect(0:pomdp.n_tests)
-POMDPs.obsindex(pomdp::SurveillancePOMDP, o::Int64) = o+1
+POMDPs.obsindex(pomdp::SurveillancePOMDP, o::Int64) = o + 1
 
 function POMDPs.observation(pomdp::SurveillancePOMDP, a::Symbol, s::Int64)
     if a == :sample && s < pomdp.N
         return Binomial(pomdp.n_tests, pomdp.p[s])
-    else
-        return [0]
     end
+    return Deterministic(0)
 end
 
 # Rewards
-function POMDPs.reward(pomdp::SurveillancePOMDP, s::Int64, a::Symbol)
-    if s == pomdp.N
-        return 0.0
-    end
-    
+function POMDPs.reward(pomdp::SurveillancePOMDP, s::Int64, a::Symbol)    
     r = 0.0
     if a == :sample
         r -= pomdp.sample_cost
     elseif a == :stop && s == 1
         r -= pomdp.false_alarm_cost
     end
-
     if 1 < s < pomdp.N
         r -= pomdp.delay_cost
     end
-    
     return r
 end
 
 # Belief
 POMDPs.initialstate(pomdp::SurveillancePOMDP) = Deterministic(1)
+
+POMDPs.discount(pomdp::SurveillancePOMDP) = 1.0
 
 end
