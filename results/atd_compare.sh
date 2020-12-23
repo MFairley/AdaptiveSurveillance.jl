854837045d37a208eb18332932ca0df10fcc80c6
Michael Fairley <michael.fairley@icloud.com>
2020-12-23 11:13:11 -0600

    confirm issue

--- results/atd_compare.sh
+++ results/atd_compare.sh
@@ -8,5 +8,4 @@
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=mfairley@stanford.edu
 ml julia
-# export JULIA_NUM_THREADS=$SLURM_CPUS_PER_TASK
 julia --machine-file <(srun hostname -s) -t $SLURM_CPUS_PER_TASK results/atd_compare.jl
