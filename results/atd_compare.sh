32f79a69252701cd26d0ded39225d44d576b3178
Michael Fairley <michael.fairley@icloud.com>
2020-12-23 11:00:58 -0600

    change the way threads called

--- results/atd_compare.sh
+++ results/atd_compare.sh
@@ -7,6 +7,6 @@
 #SBATCH --mem-per-cpu=1G
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=mfairley@stanford.edu
-# ml julia
-export JULIA_NUM_THREADS=$SLURM_CPUS_PER_TASK
-julia --machine-file <(srun hostname -s) results/atd_compare.jl
+ml julia
+# export JULIA_NUM_THREADS=$SLURM_CPUS_PER_TASK
+julia --machine-file <(srun hostname -s) -t $SLURM_CPUS_PER_TASK results/atd_compare.jl
