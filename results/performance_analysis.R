22caaa413c705b0521c974aa4308464098e37957
Michael Fairley <michael.fairley@icloud.com>
2021-01-01 19:23:01 -0600

    clean up

--- results/performance_analysis.R
+++ results/performance_analysis.R
@@ -5,9 +5,9 @@ library(survminer)
 results_path <- here("results", "tmp", "mfairley")
 header <- c("p1", "p2", "a0", "a1", "a2") # assuming 2 locations
 
-# Scenario: X
-read_scenario_alg <- function(alg) {
-  atd_alg.dt <- fread(paste(results_path, paste0("atd_", alg, "_1.csv"), sep="/"), col.names = header)
+# Scenario: g, p1, p2
+read_scenario_alg <- function(alg, g, p1, p2) {
+  atd_alg.dt <- fread(paste(results_path, paste0("atd_", alg, "_", g, "_", p1, "_", p2, ".csv"), sep="/"), col.names = header)
   atd_alg.dt[, t := 1:.N]
   atd_alg.dt[, alg := alg]
   return(atd_alg.dt)
@@ -21,22 +21,64 @@ individual_format <- function (atd.dt) { # assuming for location 1
   return(ind.dt)
 }
 
-read_scenario <- function() {
-  atd_constant.dt <- individual_format(read_scenario_alg("constant"))
-  atd_random.dt <-individual_format(read_scenario_alg("random"))
-  atd_thompson.dt <- individual_format(read_scenario_alg("thompson"))
-  atd_evsi.dt <- individual_format(read_scenario_alg("evsi"))
+read_scenario <- function(g, p1, p2) {
+  atd_constant.dt <- read_scenario_alg("constant", g, p1, p2)
+  atd_random.dt <- read_scenario_alg("random", g, p1, p2)
+  atd_thompson.dt <- read_scenario_alg("thompson", g, p1, p2)
+  atd_evsi.dt <- read_scenario_alg("evsi", g, p1, p2)
+  
+  atd.dt <- rbindlist(list(constant=atd_constant.dt,random=atd_random.dt,thompson=atd_thompson.dt,evsi=atd_evsi.dt), use.names = T, idcol = "alg")
+  return(atd.dt)
+}
+
+read_scenario_individual <- function(g, p1, p2) {
+  atd_constant.dt <- individual_format(read_scenario_alg("constant", g, p1, p2))
+  atd_random.dt <-individual_format(read_scenario_alg("random", g, p1, p2))
+  atd_thompson.dt <- individual_format(read_scenario_alg("thompson", g, p1, p2))
+  atd_evsi.dt <- individual_format(read_scenario_alg("evsi", g, p1, p2))
 
   atd.dt <- rbindlist(list(constant=atd_constant.dt,random=atd_random.dt,thompson=atd_thompson.dt,evsi=atd_evsi.dt), use.names = T, idcol = "alg")
   return(atd.dt)
 }
 
-survival_analysis <- function (atd_ind.dt) {
+survival_analysis <- function (atd_ind.dt, g) {
+  atd_ind.dt <- copy(atd_ind.dt)
+  atd_ind.dt[, t := t - g]
+  atd_ind.dt <- atd_ind.dt[t >= 0]
   s <- survfit(Surv(t, status) ~ alg, data = atd_ind.dt)
 }
 
-atd_ind.dt <- read_scenario()
-s <- survival_analysis(atd_ind.dt)
-ggsurvplot(s, conf.int = T)
+prob_false_alarm <- function (atd.dt, g) {
+  n <- sum(atd.dt[, a1])
+  e <- sum(atd.dt[t < g, a1])
+  p <- e / n
+  hw <- sqrt(p * (1 - p) / n)
+  return(c(p-hw, p+hw))
+}
 
+# 1, 0.01, 0.01
+prob_false_alarm(read_scenario(1, 0.01, 0.01), 1)
+s_1_1_1 <- survival_analysis(read_scenario_individual(1, 0.01, 0.01), 1)
+s_1_1_1
+#ggsurvplot(s_1_1_1, conf.int = T) # this breaks unless have access to the data
+
+# 1, 0.01, 0.02
+prob_false_alarm(read_scenario(1, 0.01, 0.02), 1)
+s_1_1_2 <- survival_analysis(read_scenario_individual(1, 0.01, 0.02), 1)
+s_1_1_2
+
+# 50, 0.01, 0.01
+prob_false_alarm(read_scenario(50, 0.01, 0.01), 50)
+s_50_1_1 <- survival_analysis(read_scenario_individual(50, 0.01, 0.01), 50)
+s_50_1_1
+
+# 50, 0.01, 0.02
+prob_false_alarm(read_scenario(50, 0.01, 0.02), 50)
+s_50_1_2 <- survival_analysis(read_scenario_individual(50, 0.01, 0.02), 50)
+s_50_1_2
+
+# 50, 0.02, 0.01
+prob_false_alarm(read_scenario(50, 0.02, 0.01), 50)
+s_50_2_1 <- survival_analysis(read_scenario_individual(50, 0.02, 0.01), 50)
+s_50_2_1
 
