918003172c69db4a696a4703c7e03db4672d1179
Michael Fairley <michael.fairley@icloud.com>
2021-01-04 17:51:47 -0600

    deleted old slurm scripts

--- results/result_scripts/atd_compare.sh
+++ results/result_scripts/atd_compare.sh
@@ -1,3 +1,14 @@
+#!/bin/bash
+#SBATCH --job-name=surveillance_atd_50_2_1
+#SBATCH --time=10:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=12
+#SBATCH --mem-per-cpu=1G
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=mfairley@stanford.edu
+ml julia
+
 julia results/result_scripts/atd_compare.jl 0.1 0.1 1 0.01 0.01 1000 1
 julia results/result_scripts/atd_compare.jl 0.1 0.1 1 0.01 0.02 30000 1
 julia results/result_scripts/atd_compare.jl 0.1 0.1 1 0.02 0.01 1000 1
